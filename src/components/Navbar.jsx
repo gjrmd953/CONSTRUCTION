5bbfe314a7219e3fd0ff47f5793cc5e69f4c6675
gjrmd953 <gjrmd953@gmail.com>
2026-01-02 01:25:48 +0600

    2rd add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import Container from './Container'
 
 const Navbar = () => {
-  return (
-    <div>
-        kjjjkjj
-    </div>
-  )
+    return (
+        <div className='py-4 bg-[#18A7B9]'>
+            <Container>
+                <div className='flex'>
+                    <div className="w-1/2">
+                        <h2>CONSTRUCTION</h2>
+                    </div>
+                    <div className="w-1/2">
+                        <ul className='flex justify-end gap-5'>
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li>Services</li>
+                            <li>Project</li>
+                        </ul>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default Navbar
