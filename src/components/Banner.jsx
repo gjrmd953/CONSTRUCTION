e623b870438863e8dc5af6ab46b942f7235d4db9
gjrmd953 <gjrmd953@gmail.com>
2026-01-09 15:20:16 +0600

    new edt

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import BanImg from "../assets/banner.png"
+import FirstImg from "../assets/first.png"
+import SecendImg from "../assets/secend.png"
+import ThirdImg from "../assets/third.png"
+import OneImg from "../assets/one.png"
+import TwoImg from "../assets/two.png"
+import ThreeImg from "../assets/three.png"
+import FourImg from "../assets/four.png"
+import FiveImg from "../assets/five.png"
 import Container from './Container'
 
 const Banner = () => {
@@ -7,8 +15,8 @@ const Banner = () => {
         <Container>
             <div>
                 <div className="flex">
-                    <div className="">
-                        <h1 className='pt-29.5 pb-10 font-pop font-bold text-[64px] w-170.5 '>
+                    <div className="w-1/2">
+                        <h1 className='pt-29.5 pb-10 font-pop font-bold text-[64px]'>
                             Masters of
                             Consistency and
                             Quality.
@@ -25,11 +33,53 @@ const Banner = () => {
                                 <h2 className='py-6 px-19.5 border-2 border-tertiary rounded-br-[45px] rounded-tl-[45px]  text-tertiary hover:font-bold cursor-pointer'>Contact Us</h2>
                             </div>
                         </div>
+
+                        <div className="flex pt-[112px] gap-10">
+                            <div className="font-pop">
+                                <div className="absolute">
+                                    <img src={FirstImg} alt="" />
+                                </div>
+                                <h2 className='text-[40px] text-primary relative'>25,356</h2>
+                                <h3 className='text-2xl text-secondary'>Projects Done</h3>
+                            </div>
+                            <div className="font-pop">
+                                <div className="absolute">
+                                    <img src={SecendImg} alt="" />
+                                </div>
+                                <h2 className='text-[40px] text-primary relative'>15,200</h2>
+                                <h3 className='text-2xl text-secondary'>Buildings Done</h3>
+                            </div>
+                            <div className="font-pop">
+                                <div className="absolute">
+                                    <img src={ThirdImg} alt="" />
+                                </div>
+                                <h2 className='text-[40px] text-primary relative'>350+</h2>
+                                <h3 className='text-2xl text-secondary'>Total Employees</h3>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="">
-                        <img src={BanImg} alt="" />
+                    <div className="w-1/2">
+                        <img className='w-full' src={BanImg} alt="" />
                     </div>
+                </div>
+
+                <div className="py-[174px] flex justify-between">
+                        <div className="">
+                            <img src={OneImg} alt="" />
+                        </div>
+                        <div className="">
+                            <img src={TwoImg} alt="" />
+                        </div>
+                        <div className="">
+                            <img src={ThreeImg} alt="" />
+                        </div>
+                        <div className="">
+                            <img src={FourImg} alt="" />
+                        </div>
+                        <div className="">
+                            <img src={FiveImg} alt="" />
+                        </div>
                 </div>
             </div>
         </Container>
