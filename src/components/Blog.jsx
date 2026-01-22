14246fb2fc34ecaa96015b2cd70bd21588567a9f
gjrmd953 <gjrmd953@gmail.com>
2026-01-22 19:48:49 +0600

    edit

---
+++ src/components/Blog.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from './Container'
+
+const Blog = () => {
+  return (
+    <div>
+        <Container>
+            <h2>
+                Lattest Blogs
+            </h2>
+        </Container>
+    </div>
+  )
+}
+
+export default Blog
