c0029d008fe041d36b3374aaf3a5278f0596fc8c
gjrmd953 <gjrmd953@gmail.com>
2026-01-22 20:27:30 +0600

    edit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Container from './Container'
+import ContImg from "../assets/contact.png"
 
 const Contact = () => {
   return (
     <div className='py-32'>
         <Container>
             <h2>Contact Us</h2>
+            <div className="flex">
+                <div className="w-1/2"></div>
+                <div className="w-1/2">
+                <img src={ContImg} alt="" />
+                </div>
+            </div>
         </Container>
     </div>
   )
