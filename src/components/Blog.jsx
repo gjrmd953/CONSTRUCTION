c6c3cc5ebea2dd28a31b047890e7d31a9291d1dd
gjrmd953 <gjrmd953@gmail.com>
2026-01-23 20:24:26 +0600

    edit

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Container from './Container'
+import BlgImg1 from "../assets/blogimg1.png"
+import BlgImg2 from "../assets/blogimg2.png"
 
 const Blog = () => {
   return (
     <div>
         <Container>
-            <h2>
+           <div className="">
+             <h2>
                 Lattest Blogs
             </h2>
+            <div className="flex justify-between">
+              <img src={BlgImg1} alt="" />
+              <img src={BlgImg2} alt="" />
+            </div>
+           </div>
         </Container>
     </div>
   )
