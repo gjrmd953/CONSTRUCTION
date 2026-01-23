69b53285e4b3b26ec5fd3a73b9c1b2bc3b0f1133
gjrmd953 <gjrmd953@gmail.com>
2026-01-23 20:31:21 +0600

    edit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Container from './Container'
+import MesImg from "../assets/message.png"
 
 const Message = () => {
     return (
         <div className='py-44'>
             <Container>
                 <h2>Message from CEO</h2>
+                <img src={MesImg} alt="" />
             </Container>
         </div>
     )
