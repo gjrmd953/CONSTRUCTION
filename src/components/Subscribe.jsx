b824a0a22621ac2b90a263bf431c29aafa081201
gjrmd953 <gjrmd953@gmail.com>
2026-01-23 20:15:05 +0600

    edit

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -5,10 +5,16 @@ const Subscribe = () => {
     return (
         <div className='py-14'>
             <Container>
-                <h2>
-                    Subscribe to our
-                    Newsletter
-                </h2>
+                <div className="text-center">
+                    <h2>
+                        Subscribe to our
+                        Newsletter
+                    </h2>
+                    <p>
+                        Age sold some full like rich new. Amounted repeated as believed in confined juvenile.
+                    </p>
+                    <button className='py-6 px-19.5 rounded-br-[45px] rounded-tl-[45px]  bg-tertiary text-white hover:font-bold cursor-pointer'>Explore</button>
+                </div>
             </Container>
         </div>
     )
