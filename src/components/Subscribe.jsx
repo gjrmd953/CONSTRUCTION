2102a6004d392ed66ebf3044155669404ce60280
gjrmd953 <gjrmd953@gmail.com>
2026-01-25 23:53:52 +0600

    edit

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -10,8 +10,8 @@ const Subscribe = () => {
                         Subscribe to our <br />
                         Newsletter
                     </h2>
-                    <p>
-                        Age sold some full like rich new. Amounted repeated as believed in confined juvenile.
+                    <p className='font-roboto text-[20px] text-secondary py-10'>
+                        Age sold some full like rich new. Amounted repeated as <br /> believed in confined juvenile.
                     </p>
                     <button className='py-6 px-19.5 rounded-br-[45px] rounded-tl-[45px]  bg-tertiary text-white hover:font-bold cursor-pointer'>Explore</button>
                 </div>
